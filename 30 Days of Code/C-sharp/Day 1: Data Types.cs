222724bd736d3da69d7dc13162d17329e4ee7c3e
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-10-01 11:24:29 +0300

    Data Types

---
+++ 30 Days of Code/C-sharp/Day 1: Data Types.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+         
+        int j;    
+        double e;
+        string NewString;
+        j = Convert.ToInt32(Console.ReadLine());
+        e = Convert.ToDouble(Console.ReadLine());
+        NewString = Console.ReadLine();
+        Console.WriteLine(j + i);
+        Console.WriteLine("{0:F1}", d + e);
+        Console.WriteLine(s + NewString);   
+
+        
+        // Declare second integer, double, and String variables.
+        
+        // Read and save an integer, double, and String to your variables.
+        
+        // Print the sum of both integer variables on a new line.
+        
+        // Print the sum of the double variables on a new line.
+        
+        // Concatenate and print the String variables on a new line
+        // The 's' variable above should be printed first.
+
+    }
