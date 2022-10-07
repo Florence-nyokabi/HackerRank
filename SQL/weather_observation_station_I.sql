f1b27b888c8e551f1f7e2bd4bb9350a9dfb3a01f
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-10-07 14:13:21 +0300

    Update weather_observation_station_I.sql

--- SQL/weather_observation_station_I.sql
+++ SQL/weather_observation_station_I.sql
@@ -1,4 +1,4 @@
---Problem Stat
+--Problem Sta
 /*
 Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
 +-------------+------------+
