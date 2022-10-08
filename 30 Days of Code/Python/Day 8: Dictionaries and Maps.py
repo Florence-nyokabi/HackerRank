acc1dde276362907729d46e13828568faed8ebf5
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-10-08 18:49:03 +0300

    Dictionaries and Maps

---
+++ 30 Days of Code/Python/Day 8: Dictionaries and Maps.py
@@ -0,0 +1,16 @@
+#!/bin/bash
+n = int(input())
+d = {}
+
+for i in range(n):
+    x = input().split()
+    d[x[0]] = x[1]
+
+while True:
+    try:
+        NAME = input()
+        if NAME in d:
+            print(NAME, "=", d[NAME], sep="")
+        else: print("Not found")
+    except:
+        break
